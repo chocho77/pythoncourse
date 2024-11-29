34033c78b8061804f0a9486bcd2cff3b4c703be4
Chavdar Momchilov <chavmom@gmail>
2024-11-29 23:17:27 +0200

    new task solution 	new task solution

---
+++ lecture02/task/Problem02.py
@@ -0,0 +1,7 @@
+def print_my_name()->None:
+    print("Chavdar Ventsislavov Momchilov")
+
+
+if __name__ == '__main__':
+    print_my_name()
+
---
+++ lecture02/task/Problem03.py
@@ -0,0 +1,12 @@
+def sol() -> None:
+    x = 3
+    y = 3.13
+    text = "Hello"
+    is_logged_in = True
+
+    print(type(x), type(y), type(text), type(is_logged_in))
+
+
+if __name__ == '__main__':
+    sol()
+
