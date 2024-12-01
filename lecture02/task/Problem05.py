d4b07ee5532d429efb7229c466d920028956253d
Chavdar Momchilov <chavmom@gmail>
2024-12-01 14:11:02 +0200

    new file:   lecture02/task/Problem05.py

---
+++ lecture02/task/Problem05.py
@@ -0,0 +1,12 @@
+def sol():
+    # area triangle = h * a / 2
+    h_a = 13.66
+    a = 245.54
+    area = h_a * a / 2
+    print('area triangle = ', area)
+    pass
+
+
+if __name__ == '__main__':
+    sol()
+
