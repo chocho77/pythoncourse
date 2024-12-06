1c0ac45681d60227f76ddfc4896cb242e4c16a65
Chavdar Momchilov <chavmom@gmail>
2024-12-06 10:24:07 +0200

    Changes to be committed: 	new file:   lecture02/examples/HelloSourceClass.py 	new file:   lecture02/examples/pkg/SourceClass.py 	new file:   lecture02/examples/pkg/__init__.py 	new file:   lecture02/examples/pkg/__pycache__/SourceClass.cpython-312.pyc 	new file:   lecture02/examples/pkg/__pycache__/__init__.cpython-312.pyc

---
+++ lecture02/examples/HelloSourceClass.py
@@ -0,0 +1,10 @@
+from pkg.SourceClass import SourceClass
+
+def sol():
+    sourceClass = SourceClass()
+
+
+if __name__ == '__main__':
+    sol()
+
+
---
+++ lecture02/examples/pkg/SourceClass.py
@@ -0,0 +1,4 @@
+class SourceClass:
+    def __init__(self):
+        print("In Constructor: class->SourceClass")
+
